66ef13dc89c22c5ac8228ac48ee1f88b73c9eca7
thefloydshowvic <thefloydshowvic@gmail.com>
2026-08-07 13:05:11 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE = 'the-floyd-show-hq-v22';
+
+const SHELL = [
+  './',
+  './index.html',
+  './config.js?v=22',
+  './manifest.webmanifest',
+  './icons/icon-192.png?v=22',
+  './icons/icon-512.png?v=22',
+  './icons/apple-touch-icon.png?v=22',
+  './icons/favicon-32.png?v=22',
+  './icons/favicon-16.png?v=22',
+  './icons/favicon.ico?v=22'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || network;
+    })
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
